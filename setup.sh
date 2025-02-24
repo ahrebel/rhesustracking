e501c0e5f9f6ff394f095584f70d2697145eeb6a
Anthony Rebello <anthonyhrebello@gmail.com>
2025-02-23 23:03:35 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
 
+echo "Creating virtual environment..."
+python3 -m venv venv
+source venv/bin/activate
+echo "Installing dependencies..."
+pip install --upgrade pip
+pip install -r requirements.txt
+echo "Setup complete. Activate the environment with 'source venv/bin/activate'"

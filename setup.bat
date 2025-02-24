1bd97739ba9f95539e3f6a1f5e41740a02af6717
Anthony Rebello <anthonyhrebello@gmail.com>
2025-02-23 23:03:50 -0500

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,1 +1,12 @@
+@echo off
+REM setup.bat - for Windows
+
+echo Creating virtual environment...
+python -m venv venv
+call venv\Scripts\activate
+echo Installing dependencies...
+pip install --upgrade pip
+pip install -r requirements.txt
+echo Setup complete. Activate the environment with "venv\Scripts\activate"
+pause
 

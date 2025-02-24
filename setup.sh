db7eebe366dd7939dacb663a26b42fe10678049f
Anthony Rebello <anthonyhrebello@gmail.com>
2025-02-23 23:39:35 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-conda install -c conda-forge pytables
-echo "Creating virtual environment..."
+echo "Creating (or updating) a virtual environment for pip dependencies..."
 python3 -m venv venv
 source venv/bin/activate
-echo "Installing dependencies..."
+echo "Installing pip dependencies..."
 pip install --upgrade pip
 pip install -r requirements.txt
-echo "Setup complete. Activate the environment with 'source venv/bin/activate'"
+echo "Setup complete. Activate the environment with 'source venv/bin/activate' within your Conda environment."

fe7700c2d260a9fefcdc653d7689ad4928b6d99a
Anthony Rebello <anthonyhrebello@gmail.com>
2025-02-24 11:56:35 -0500

    Delete setup.sh

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Creating (or updating) a virtual environment for pip dependencies..."
-python3 -m venv venv
-source venv/bin/activate
-echo "Installing pip dependencies..."
-pip install --upgrade pip
-pip install -r requirements.txt
-echo "Setup complete. Activate the environment with 'source venv/bin/activate' within your Conda environment."

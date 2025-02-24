e21c2ab96c0444fa1334eb7c7b270d7be0de6001
Anthony Rebello <anthonyhrebello@gmail.com>
2025-02-23 23:31:35 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "Creating virtual environment..."
 conda install -c conda-forge pytables
+echo "Creating virtual environment..."
 python3 -m venv venv
 source venv/bin/activate
 echo "Installing dependencies..."
